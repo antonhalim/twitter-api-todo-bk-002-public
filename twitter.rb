c280ba9746c1c6e3825bae03d6923da914700ce7
axton21 <axton_n@yahoo.com>
2015-03-01 22:02:39 -0500

    done

--- twitter.rb
+++ twitter.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'yaml'
+require 'pry'
 
 class TwitterApi
   attr_reader :client
@@ -16,14 +17,14 @@ class TwitterApi
   end
 
   def most_recent_follower
-    # find the Twitter gem method that accomplishes this!
+      client.friends.first
   end
 
   def find_user_for(username)
-    # find the Twitter gem method that accomplishes this!
+      client.user(username)
   end
 
   def find_followers_for(user)
-    # find the Twitter gem method that accomplishes this, and limit it to 10 followers only!
+      client.followers.take(10)
   end
 end
